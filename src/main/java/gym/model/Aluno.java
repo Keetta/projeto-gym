51d97b301c5cba6318592d62e2cac91561ea0083
Evandro-Meurer <evandrofriedrichmeurer237@gmail.com>
2026-03-04 20:28:16 -0300

    Fiz esse começo

--- src/main/java/gym/model/Aluno.java
+++ src/main/java/gym/model/Aluno.java
@@ -1,2 +1,16 @@
 package gym.model;
 
+
+public class Aluno {
+    private int id;
+    private String nome;
+    private int idade;
+    private Plano plano;
+
+    public Aluno(int id, String nome, int idade, Plano plano) {
+
+
+
+
+    }
+}

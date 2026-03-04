32ea5f1804278421357a335b924238e0c1ec0456
Keetta <nicholasfelixloko2@gmail.com>
2026-03-04 19:26:18 -0300

    Added Plano Structure

--- src/main/java/gym/model/Plano.java
+++ src/main/java/gym/model/Plano.java
@@ -1,1 +1,21 @@
 package gym.model;
+
+public abstract class Plano {
+    private String nome;
+    private double valorBase;
+
+    public Plano(String nome, double valorBase){
+        this.nome = nome;
+        this.valorBase = valorBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValorBase() {
+        return valorBase;
+    }
+
+    public abstract double calcularMensalidade();
+}
